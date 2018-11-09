bb7d09d59c88c606c1327e45bec8c17fa71570b7
3163300617 <solerherediacgmail.com>
2018-11-09 12:53:46 -0500

    Initial commit

---
+++ modulo2/js/cuarto.js
@@ -0,0 +1,5 @@
+var number=parseInt(prompt("ingrese el numero"));
+while (isNaN(number) || number<=0){
+    numero=prompt("ingrese el numero");
+
+}
---
+++ modulo2/js/primero.js
@@ -0,0 +1,21 @@
+
+var Money = prompt ("ingrese el valor que va a depositar");
+var interest=Money*Math.pow(1.02,60);
+document.write("<h1>valor consignado:"+Money+"</h1>");
+document.write("<h1>Valor futuro en 5 años:"+interest+"</h1>");
+
+
+
+
+
+
+
+   
+   var number=parseInt(prompt("ingrese el numero"));
+   while (isNaN(number) || number<=0){
+       numero=prompt("ingrese el numero");
+
+   }
+   
+
+
